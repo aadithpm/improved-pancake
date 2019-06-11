5296706eab99349497ccb2529b0f2b1f15e30aaa
Aadith Madapusi <amadapusi@industrydive.com>
2019-06-11 16:13:00 -0400

    Added test for checking if empty Item is inserted -- refactored home view

--- lists/views.py
+++ lists/views.py
@@ -2,9 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from lists.models import Item
 
 
 def home_page(request):
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': new_item_text,
     })
