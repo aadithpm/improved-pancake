892ae5932d0047b16521c01d256ab264298f8ac4
Aadith Madapusi <amadapusi@industrydive.com>
2019-06-13 09:09:54 -0400

    Working on changing URL for lists

--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,7 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/list')
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
