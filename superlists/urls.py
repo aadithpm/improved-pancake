5a172d0ca7f05cb7c56f62a09994acc921efce54
Aadith Madapusi <amadapusi@industrydive.com>
2019-06-13 09:30:51 -0400

    Updated URL regex to include URLs both ending with slash and without

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,5 @@ from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/list/$', views.view_list, name='view_list'),
+    url(r'^lists/list/{0,1}$', views.view_list, name='view_list'),
 ]
