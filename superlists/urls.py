93e25a6ae7019bd336c8bd3805a367443d36a8f0
Aadith Madapusi <amadapusi@industrydive.com>
2019-06-14 08:55:36 -0400

    Moving to new URL for creating a list item - refactoring tests

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,4 +20,5 @@ from lists import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/list/{0,1}$', views.view_list, name='view_list'),
+    url(r'^lists/new/{0,1}$', views.new_list, name='new_list')
 ]
