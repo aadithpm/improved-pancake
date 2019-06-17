280f9c62bbfbbb63974fafefab68108143276360
Aadith Madapusi <amadapusi@industrydive.com>
2019-06-17 08:45:51 -0400

    Added URL and view for adding new item - made relevant changes in template and added more tests

--- lists/tests.py
+++ lists/tests.py
@@ -82,3 +82,34 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_item(self):
+        first_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{first_list.id}/add_item',
+            data={'item_text': 'Item for existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'Item for existing list')
+        self.assertEqual(new_item.list, first_list)
+
+    def test_redirects_to_list_view(self):
+        first_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{first_list.id}/add_item',
+            data={'item_text': 'Item for existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{first_list.id}/')
+
+    def test_passes_correct_list_to_template(self):
+        first_list = List.objects.create()
+        response = self.client.get(f'/lists/{first_list.id}/')
+        self.assertEqual(response.context['list'], first_list)
