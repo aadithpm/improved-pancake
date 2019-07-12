187a746d31fe8170054794e5d2d55ed6c2fb6d22
Aadith Madapusi <amadapusi@industrydive.com>
2019-07-12 10:02:07 -0400

    Adding error message to form

--- lists/forms.py
+++ lists/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 
+from lists.models import Item
 
-class ItemForm(forms.Form):
-    item_text = forms.CharField(
-        widget=forms.fields.TextInput(attrs={
-            'placeholder': 'Enter a to-do item',
-            'class': 'form-control'
-        })
-    )
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control'
+            }),
+        }
+        error_messages = {
+            'text': {'required': "You can't have an empty list item"}
+        }
