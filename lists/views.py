ac643a90207fd4b75723c5d151ed35ad5aaf0eae
Aadith Madapusi <amadapusi@industrydive.com>
2019-07-15 09:07:37 -0400

    unwanted import

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from lists.models import Item, List
+from lists.models import List
 from lists.forms import ItemForm
 
 
